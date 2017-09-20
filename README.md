fc76c76b68df550bc27eaf54e3ffdc1223b1fab0
Noah Altunian <nbaltunian@gmail.com>
2017-09-19 21:15:13 -0400

    Bump to v0.1.0
    Format Total Market Cap to the thousandth place, and fix some code discrepancies.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Pynancier
 ## A Python-implemented [Financier](https://github.com/naltun/financier) clone.
 
+Pynancier is a Python-implemented clone of my other script, [Financier](github.com/naltun/financier), which is written in PHP.
+
+### Why
+Because I check [CoinMarketCap](coinmarketcap.com) daily, and frequently, but I hate leaving the terminal unnecessarily. As a result, I developed Financier, and Pynancier, so that I can keep this script handy in the terminal and not have to leave (drive-by-keyboard ftw).
+
+### TODO
+I'm needing to add some more functionality to this script, as well as improve the documentation. Currently, I have comments in the code _loosely_ walking people through what's going on. It's a small script, to be honest. The documentation will most likely exist within the README and will simply explain some of the arguments which can be supplied to the script. Namely, you can specify an integer argument which displays the top `N' performing cryptocurrencies and accompanying information.
+
+Preferably, I can add curses to the script so that the user can specify any number of currencies they want and scroll through them accordingly. If that's the case, then I can get rid of the `clear' function altogether. Things for the future. The most immediately planned feature is to specify a maximum of ~6 as the command-line argument for the script, so that all information can be displayed equally.
+
+### DISCLAIMER
+I'm not a Python hacker. Heck, I'm not even a _Pythonista_. I took this project on (and will probably make this my _main_ script for cryptocurrency) as a result of taking on the task of learning Python at work. Want to learn a new language? _Build_ something. Hence, _Pynancier_.
+
+If anyone has any Pythonic feedback, I'd very much appreciate any. :) I'd be sure to give that expertise back to the community.
+
+### LICENSE
+GPLv2 -- the lovely, libre-loving license most of us are familiar with. For more information on Free/Libre, Open Source Software, please consult [Wikipedia](en.wikipedia.org/wiki/Free_software).
+
--- pynancier.py
+++ pynancier.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python
 
 # Pynancier is a Python script for delivering real-time, up-to-date Cryptocurrency market data
 # Pynancier is a portmanteau of *Python* and *Financier*
@@ -19,15 +19,17 @@ import requests
 import time
 from subprocess import call
 
-version = '0.0.2.1'
+version = '0.1.0'
 
-# Financier ASCII logo; made with figlet.js (github.com/patorjk/figlet.js)
-logo = """_______ _                        _
-|  ____(_)                      (_)
-| |__   _ _ __   __ _ _ __   ___ _  ___ _ __
-|  __| | | '_ \ / _` | '_ \ / __| |/ _ \ '__|
-| |    | | | | | (_| | | | | (__| |  __/ |
-|_|    |_|_| |_|\__,_|_| |_|\___|_|\___|_| v%s
+# Pynancier ASCII logo; made with figlet.js (github.com/patorjk/figlet.js)
+logo = """  _____                              _
+ |  __ \                            (_)
+ | |__) |   _ _ __   __ _ _ __   ___ _  ___ _ __
+ |  ___/ | | | '_ \ / _` | '_ \ / __| |/ _ \ '__|
+ | |   | |_| | | | | (_| | | | | (__| |  __/ |
+ |_|    \__, |_| |_|\__,_|_| |_|\___|_|\___|_| v%s
+         __/ |
+        |___/
 """ % (version)
 
 # where we're actually getting our information from
@@ -43,15 +45,14 @@ def get_global_data():
     global_data_raw  = requests.get(global_data_url)
     global_data      = global_data_raw.json()
 
-    # I need to format the number below to be more financially readable
-    # This is actually proving quite difficult. In PHP, I would simply `number_format(value, decimal_places)'
-    total_market_cap  = global_data['total_market_cap_usd']
+    # let's format that exceptionally large number to include commas, shall we?
+    total_market_cap  = '{0:,}'.format(global_data['total_market_cap_usd'])
     bitcoin_dominance = global_data['bitcoin_percentage_of_market_cap']
     active_currencies = global_data['active_currencies']
 
-    print("Total Market Cap:       $%s" % total_market_cap)
-    print("Bitcoin Dominance:       %s" % bitcoin_dominance)
-    print("Total Active Currencies: %s" % active_currencies)
+    print("Total Market Cap:        $%s" % total_market_cap)
+    print("Bitcoin Dominance:       %s"  % bitcoin_dominance)
+    print("Total Active Currencies: %s"  % active_currencies)
 
 # patience is a virtue -- here it allows the script to cease making any additional web requests for 60 seconds so I can read the darn data
 def wait():
